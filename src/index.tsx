b9a9987d59e5d5949ccf3503c2a904bd64019320
jide creppy <jide.creppy@gmail.com>
2024-08-15 15:12:26 +0100

    add base dashboard template

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './style.css';
 // import reportWebVitals from './reportWebVitals';
 import App from './components/App';
+import Dashboard from './components/Dashboard/Dashboard';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    {/* <Dashboard/> */}
   </React.StrictMode>
 );
 
